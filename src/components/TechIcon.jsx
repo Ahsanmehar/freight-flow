432faec6ed7c4fe28486be876befeb38ea4461ab
Areebusman27 <mahadusman2008@gmail.com>
2025-09-14 18:22:46 +0500

    My first commit

--- src/components/TechIcon.jsx
+++ src/components/TechIcon.jsx
@@ -1,12 +1,29 @@
 import {
-  Home,
-  Send,
+  Home, Send, UserRoundCheck, Ellipsis, Eye, Filter, Map, FileText, Truck, DollarSign, Settings, Activity, Search, CircleQuestionMark, MessageCircle, Mail, Phone, Shield, Users, Package
 } from "lucide-react";
 
 const TechIcon = ({ name, className = "" }) => {
   const icons = {
     home: <Home className={className} />,
     send: <Send className={className} />,
+    roundcheck: <UserRoundCheck className={className} />,
+    elipsis: <Ellipsis className={className} />,
+    eye: <Eye className={className} />,
+    filter: <Filter className={className} />,
+    map: <Map className={className} />,
+    file: <FileText className={className} />,
+    truck: <Truck className={className} />,
+    dollar: <DollarSign className={className} />,
+    settings: <Settings className={className} />,
+    activity: <Activity className={className} />,
+    search: <Search className={className} />,
+    question: <CircleQuestionMark className={className} />,
+    message: <MessageCircle className={className} />,
+    mail: <Mail className={className} />,
+    phone: <Phone className={className} />,
+    shield: <Shield className={className} />,
+    users: <Users className={className} />,
+    package: <Package className={className} />,
   };
 
   return icons[name.toLowerCase()] || <span className={className}>{name}</span>;
